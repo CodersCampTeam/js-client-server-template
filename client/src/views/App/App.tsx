d14c53958987a4796bcdf4f4b0bcf364b5a40196
Magdalena <sochamagdalena8@gmail.com>
2021-04-23 14:53:05 +0200

    Chakra and gsap

--- client/src/views/App/App.tsx
+++ client/src/views/App/App.tsx
@@ -2,6 +2,18 @@ import axios from 'axios';
 import React, { ReactElement, useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NotFound from '../../components/NotFound';
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+
+// customized colors,fonts, basically everything for Chakra (optional)
+const colors = {
+    brand: {
+      900: "#1a365d",
+      800: "#153e75",
+      700: "#2a69ac",
+    },
+  }
+  const customTheme = extendTheme({ colors });
+
 
 export const Home = (): ReactElement => {
 
@@ -24,16 +36,20 @@ export const Home = (): ReactElement => {
 };
 
 
+
+
 const App = (): ReactElement => {
 
 
     return (
         <div>
             <BrowserRouter>
-                <Switch>
-                    <Route exact path="/"  component={Home} />
-                    <Route component={NotFound} />
-                </Switch>
+                <ChakraProvider theme={customTheme}>
+                    <Switch>
+                        <Route exact path="/"  component={Home} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </ChakraProvider>
             </BrowserRouter>
         </div>
     );
